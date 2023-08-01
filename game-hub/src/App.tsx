111f3a868c35f951635e186ac8117e06c0b506e2
mohamed nishan <mohamednishanm@gmail.com>
2023-08-01 14:10:03 +0530

    Build a navbar

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,4 +1,5 @@
 import { Grid, GridItem,Show } from "@chakra-ui/react";
+import NavBar from "./components/Navbar";
 // import NavBar from "./components/NavBar";
 // import GameGrid from "./components/GameGrid";
 
@@ -12,7 +13,9 @@ function App() {
       lg: `"nav nav" "aside main"`,
     }}
   >
-      <GridItem area='nav' bg='coral'>Nav</GridItem>
+      <GridItem area='nav'>
+        <NavBar />
+      </GridItem>
       <Show above="lg">
         <GridItem area="aside" bg={"gold"}>
           Aside
